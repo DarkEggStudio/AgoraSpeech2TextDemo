881deddb1d92eb5dd9242dafe37755197cf7eb41
darkzero <darkzero_mk2@hotmail.com>
2024-05-17 12:26:33 +0800

    Update vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -13,14 +13,6 @@ export default defineConfig({
     // port: 5173,
     // port: 5173,
     proxy: {
-      '^/rtt-staging': {
-        target: 'http://60.191.137.172:16000', //'https://stt-demo-staging.agora.io',
-        secure: false,
-        changeOrigin: true,
-        WS: true,
-        // PathRewrite: { '^/rtt-staging': '/api' }
-        rewrite: (path) => path.replace(/^\/rtt-staging/, '/api')
-      },
       '^/rtt-prod': {
         target: 'https://api.agora.io',
         secure: false,
